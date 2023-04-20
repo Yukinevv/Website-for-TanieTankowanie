96e6438891c010e68a0c79a46d3a7ef5e4351f2b
Adrian Rodzic <adrianrodzic33@gmail.com>
2023-04-20 10:40:30 +0200

    Poprawka w funkcji handleResize

--- main.js
+++ main.js
@@ -1,21 +1,23 @@
-// Pobranie referencji do elementu div
 let tileDivs = document.getElementsByClassName("tile");
 let tileDivsLeftHeaders = document.querySelectorAll(".left-side2 h3");
 let tileDivsRightHeaders = document.querySelectorAll(".right-side2 h3");
 let tileDivsLeftParagraphs = document.querySelectorAll(".left-side2 p");
 let tileDivsRightParagraphs = document.querySelectorAll(".right-side2 p");
+let change = true;
 
 function handleResize() {
-    var windowWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    let windowWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
 
-    if (windowWidth <= 1260) {
+    if (windowWidth <= 1350 && change) {
         [tileDivs[1].innerHTML, tileDivs[2].innerHTML] = [tileDivs[2].innerHTML, tileDivs[1].innerHTML];
         [tileDivsLeftHeaders[1].innerHTML, tileDivsRightHeaders[0].innerHTML] = [tileDivsRightHeaders[0].innerHTML, tileDivsLeftHeaders[1].innerHTML];
         [tileDivsLeftParagraphs[1].innerHTML, tileDivsRightParagraphs[0].innerHTML] = [tileDivsRightParagraphs[0].innerHTML, tileDivsLeftParagraphs[1].innerHTML];
-    } else {
-        [tileDivs[2].innerHTML, tileDivs[1].innerHTML] = [tileDivs[1].innerHTML, tileDivs[2].innerHTML];
-        [tileDivsRightHeaders[0].innerHTML, tileDivsLeftHeaders[1].innerHTML] = [tileDivsLeftHeaders[1].innerHTML, tileDivsRightHeaders[0].innerHTML];
-        [tileDivsRightParagraphs[0].innerHTML, tileDivsLeftParagraphs[1].innerHTML] = [tileDivsLeftParagraphs[1].innerHTML, tileDivsRightParagraphs[0].innerHTML];
+        change = false;
+    } else if (windowWidth > 1350 && !change) {
+        [tileDivs[1].innerHTML, tileDivs[2].innerHTML] = [tileDivs[2].innerHTML, tileDivs[1].innerHTML];
+        [tileDivsLeftHeaders[1].innerHTML, tileDivsRightHeaders[0].innerHTML] = [tileDivsRightHeaders[0].innerHTML, tileDivsLeftHeaders[1].innerHTML];
+        [tileDivsLeftParagraphs[1].innerHTML, tileDivsRightParagraphs[0].innerHTML] = [tileDivsRightParagraphs[0].innerHTML, tileDivsLeftParagraphs[1].innerHTML];
+        change = true;
     }
 }
 
